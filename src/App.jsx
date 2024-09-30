c011e6d41a65bbf79f964e578607fe49c1284775
seafood <mail.seafood@proton.me>
2024-09-30 21:59:32 +0300

    Added Collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import Features from "./components/Features.jsx";
+import Collaboration from "./components/Collaboration.jsx";
 
 const App = () => {
   return (
@@ -8,7 +9,8 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        <Features/>
+        <Features />
+        <Collaboration />
       </div>
     </>
   );
